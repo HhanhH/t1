ce34c327d152c9cff43a88bc1845d64faa3645cc
workbench <>
2020-12-18 20:06:08 +0800

    {/src/main/java/com/myspace/t1/EnumHttpTest.java}

--- src/main/java/com/myspace/t1/EnumHttpTest.java
+++ src/main/java/com/myspace/t1/EnumHttpTest.java
@@ -6,7 +6,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
@@ -17,19 +16,29 @@ import java.util.List;
  * @Date 2020/12/18 17:57
  */
 public class EnumHttpTest {
-    public List<Long> getList() {
-        String url = "http://localhost:8001/demo/list/getList";
-        URI uri = URI.create(url);
-        HttpGet httpGet = new HttpGet(uri);
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        try {
-            CloseableHttpResponse response = httpClient.execute(httpGet);
-            String result = EntityUtils.toString(response.getEntity());
-            List<Long> res = JSON.parseCollection(result, List.class, Long.class);
-            return res;
-        } catch (IOException e) {
-            return Collections.emptyList();
-        }
-    }
+
+	public List<Long> list() {
+		List<Long> list = new ArrayList<>();
+		list.add(1L);
+		list.add(2L);
+		return list;
+	}
+	public List<Long> getList() {
+		String url = "http://localhost:8001/demo/list/getList";
+		URI uri = URI.create(url);
+		HttpGet httpGet = new HttpGet(uri);
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		try {
+			CloseableHttpResponse response = httpClient.execute(httpGet);
+			String result = EntityUtils.toString(response.getEntity());
+			List<Long> res = JSON.parseCollection(result, List.class,
+					Long.class);
+			return res;
+		} catch (IOException e) {
+			return Collections.emptyList();
+		}
+	}
+	public EnumHttpTest() {
+	}
 
 }
