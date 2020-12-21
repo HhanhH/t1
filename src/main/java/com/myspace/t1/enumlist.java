72f0a832ead9405b0e375c62e392c729ba485f00
workbench <>
2020-12-21 11:02:33 +0800

    {/src/main/java/com/myspace/t1/enumlist.java}

--- src/main/java/com/myspace/t1/enumlist.java
+++ src/main/java/com/myspace/t1/enumlist.java
@@ -6,12 +6,16 @@ package com.myspace.t1;
 
 public class enumlist implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public enumlist() {
-    }
+	public enumlist() {
+	}
 
-
-
+	public List<String> list() {
+		List<String> list = new ArrayList<>();
+		list.add("1L");
+		list.add("2L");
+		return list;
+	}
 
 }
